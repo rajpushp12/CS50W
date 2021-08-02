325df3ce36331e36410e5c7da857d0ee0e91c437
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2021-08-03 02:06:05 +0530

    complete

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.info, name="info"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random/", views.random, name="random")
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse,HttpResponseRedirect
 from markdown2 import Markdown
+from random import choice
 from . import util
 
 
@@ -15,7 +15,10 @@ def info(request, title):
     content=util.get_entry(title)
 
     if(not content):
-        return HttpResponse("Error 404, the requested page doesn't exist")
+        return render(request, 'encyclopedia/error.html', {
+            "status":"404",
+            "message":"The requested page doesn't exist"
+        })
 
     markdowner=Markdown()
     html_content=markdowner.convert(content)
@@ -28,14 +31,65 @@ def info(request, title):
 def search(request):
 
     key=request.POST.get('q','')
-
     entries=util.list_entries()
-    true=key in entries
 
-    if true:
+    if key in entries:
         return redirect('info', title=key)
     
-    #filtered_entries=
-    return render(request, 'encyclopedia/index.html')
+    suggest=[entry for entry in entries if not entry.find(key) == -1]
+    return render(request, 'encyclopedia/index.html', {
+        "entries":suggest
+    })
+
+def create(request):
+    if request.method=="GET":
+        return render(request, "encyclopedia/create.html")
+    
+    if request.method=="POST":
+        input_title=request.POST.get('title', '')
+        input_info=request.POST.get('info', '')
+
+        if len(input_title)==0:
+            return render(request, 'encyclopedia/error.html', {
+            "status":"400",
+            "message":"Please enter the title"
+        })
+
+        if util.get_entry(input_title):
+            return render(request, 'encyclopedia/error.html', {
+            "status":"400",
+            "message":"This page already exists"
+        })
+
+        util.save_entry(input_title,input_info)
+        return redirect('info', title=input_title)
+
+
+def edit(request, title):
+    if request.method=="GET":
+        current_info=util.get_entry(title)
+        return render(request, 'encyclopedia/edit.html', {
+            "title": title,
+            "md_info": current_info
+            })
+
+    if request.method=="POST":
+        title=request.POST.get('title','')
+        updated_info=request.POST.get('info','')
+        
+        util.save_entry(title,updated_info)
+        return redirect('info', title=title)
+
+def random(request):
+    entries=util.list_entries()
+    select=choice(entries)
+
+    return redirect('info', title=select)
+
+
+
+
+
+
 
 

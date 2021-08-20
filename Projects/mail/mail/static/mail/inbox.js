96ff165c2657d88b6e346e74485ef7f4fe92be40
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2021-08-20 10:22:56 +0530

    update

---
+++ Projects/mail/mail/static/mail/inbox.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').onsubmit = send_mail;
+  
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+
+
+
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+
+
+function send_mail(){
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+  load_mailbox('sent');
+  return false;
+}
+
+
+
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+
+    emails.forEach( email => {
+
+      var div1=document.createElement('div');
+      div1.className="jumbotron";
+
+      div1.innerHTML=`
+      <div><h5>Subject: ${email.subject}</h5></div>
+      <div>${email.recipients}</h1>
+      <div>${email.timestamp}</div>
+      `;
+
+      document.querySelector('#emails-view').appendChild(div1);
+    });
+  });
+
+}
--- Python Intro/lambda.py
+++
@@ -1,9 +0,0 @@
-people = [
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Cho", "house": "Ravenclaw"},
-    {"name": "Draco", "house": "Slytherin"}
-]
-
-people.sort(key=lambda person: person["house"])
-
-print(people)

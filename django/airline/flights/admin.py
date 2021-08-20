eef4797d8ddc33264372980c68d933d32cd2eae8
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2021-08-20 10:25:57 +0530

    update

--- django/airline/flights/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class FlightAdmin(admin.ModelAdmin):
-	list_display=("id","origin","destination","duration")
-
-admin.site.register(Flight, FlightAdmin)
-admin.site.register(Airport)
-admin.site.register(Passenger)

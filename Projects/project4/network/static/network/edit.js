60af3158eaaf26d6f06bdc1f6b1c5c4611d91abd
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2021-09-02 00:42:46 +0530

    completed api and loading

--- Projects/project4/network/static/network/edit.js
+++ Projects/project4/network/static/network/edit.js
@@ -1,8 +1,44 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#edit-btn').addEventListener('click', () => edit_load());
+    document.querySelectorAll('#edit-btn').forEach(function(button){
+        button.onclick = () => edit_load(button.value);
+    });
+
+    //Default
+    index_load();
 });
 
-function edit_load() {
-    document.querySelector('#index-view').style.display = 'none';
 
+
+function index_load() {
+    document.querySelector('#index-view').style.display = 'block';
+    document.querySelector('#edit-view').style.display = 'none';
+}
+
+
+
+function edit_load(id) {
+    document.querySelector('#index-view').style.display = 'none';
+    document.querySelector('#edit-view').style.display = 'block';
+
+    fetch(`/post/${id}`)
+    .then(response => response.json())
+    .then(post => {
+        console.log(post);
+
+        document.querySelector('#edit-body').innerHTML = post.post_content;
+    });
+    document.querySelector('#edit-form').onsubmit = post_update(id);
+}
+
+
+
+function post_update(id){
+    const content = document.querySelector('#edit-body').value
+    fetch(`/post/${id}` ,{
+        method: 'PUT',
+        body: JSON.stringify({
+            post_content: content
+        })
+    })
+    return false;
 }
